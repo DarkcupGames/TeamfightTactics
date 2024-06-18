87228c6b56210fd14e7c183b0e34d9885b217c1c
Bình <kaisne0708@gmail.com>
2024-06-18 13:57:04 +0700

    Setup scene

---
+++ Assets/Scripts/GameInstaller.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+public class GameInstaller : MonoInstaller
+{
+    [Inject] private GamePlayController gamePlayController;
+    public override void InstallBindings()
+    {
+        Container.Bind<GamePlayController>().FromComponentInHierarchy().AsSingle();
+        Container.Bind<GameData>().FromComponentInHierarchy().AsSingle();
+        Container.BindFactory<GamePlayController, FactoryGamePlayController>().FromComponentInNewPrefabResource(nameof(GamePlayController)).AsSingle();
+
+    }
+}
+public class FactoryGamePlayController : PlaceholderFactory<GamePlayController>
+{
+}
+public interface IGamePlayController
+{
+    GameStage CurrentGameStage { get; set; }
+    GameObject[,] GridChampionsArray { get; set; }
+    List<ChampionBonus> ActiveBonusList { get; set; }
+    bool BuyChampionFromShop(Champion champion);
+    void StartDrag();
+    void StopDrag();
+    void Buylvl();
+    void TakeDamage(int damage);
+    void RestartGame();
+    void EndRound();
+    void OnChampionDeath();
+}
